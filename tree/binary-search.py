1fff9743ad60a8ebfbaf2b60eecdaba027aa08a5
tanjimulislam55 <tanjimulislam118@gmail.com>
2022-08-22 14:18:01 +0600

    inorder traversal

--- tree/binary-search.py
+++ tree/binary-search.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class TreeNode:
     def __init__(self, data) -> None:
         self.data = data
@@ -9,7 +12,7 @@ class Tree:
     def __init__(self, root) -> None:
         self.root = root
 
-    def insert(self, data):
+    def insert(self, data: int) -> None:
         temp: TreeNode = self.root
 
         while True:
@@ -26,11 +29,41 @@ class Tree:
                     temp.right = TreeNode(data)
                     break
 
-    def search(self, data):
+    def insert_using_stack(self, data):
+        stack = List[self.root]
+
+        while stack:
+            item: TreeNode = stack.pop()
+            if item.data > data:
+                pass
+            else:
+                pass
+
+    def search(self, data: int) -> None:
         pass
 
-    def update(self, data):
+    def inorder_traversal_using_recursion(self, root: TreeNode):
+        if root is None:
+            return
+        if root.left:
+            self.inorder_traversal(root.left)
+        print(root.data)
+        if root.right:
+            self.inorder_traversal(root.right)
+        return
+
+    def update(self, data: int) -> None:
         pass
 
-    def delete(self, data):
+    def delete(self, data: int) -> None:
         pass
+
+
+# [100, 7, 3, 50, 99, 101]
+t1 = Tree(TreeNode(100))
+t1.insert(7)
+t1.insert(3)
+t1.insert(50)
+t1.insert(99)
+t1.insert(101)
+print(t1.inorder_traversal(t1.root))
